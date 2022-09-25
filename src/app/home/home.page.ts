d97a4666267298cb8c28c9f14b2e1f6a44f8cf5d
markos3d <markost3d@gmail.com>
2022-09-25 19:01:03 +0200

    receive message through websocket in angular

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../shared/services/message.service';
+import { WebsocketService } from '../shared/services/websocket.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  message = '';
 
-  constructor() {}
+  constructor(private messageService: MessageService, private websocketService: WebsocketService) { }
+
+  ngOnInit() {
+    this.connect();
+  }
+  connect(): void {
+    this.websocketService.connect();
+
+    // subscribe receives the value.
+    this.messageService.message.subscribe((data) => {
+      this.message = data.MESSAGE;
+    });
+  }
+
+  disconnect(): void {
+    this.websocketService.disconnect();
+  }
 
 }
